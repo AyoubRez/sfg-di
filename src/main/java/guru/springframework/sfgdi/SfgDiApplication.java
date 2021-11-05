8b97cc369eff0c26494fba7a590d23610555c258
Rezzouk Ayoub <AREZZOUK@pca.ma>
2021-11-05 11:54:53 +0100

    Spring context

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,26 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        // Getting the application context
+        ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+        // Getting the MyController controller from the application context
+        MyController myController = (MyController) ctx.getBean("myController");//Casting to MyController
+
+        //Calling The sayHello() methode in the MyController
+        String greeting = myController.sayHello();
+
+        //Printing the returned value to the console
+        System.out.println(greeting);
+    }
 
 }
